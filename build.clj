b9140dafdc9511965d2014aa8ec0c33bd0916390
mn-dimension <thecontinium+mn.dimension@outlook.com>
2024-06-20 15:48:59 +0100

    deploy to clojars

--- build.clj
+++ build.clj
@@ -1,7 +1,8 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
 
-(def lib 'dimension/plotly-pyclj)
+(def lib 'io.github.mn-dimension/plotly-pyclj)
 (def version "0.1.9")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn" :aliases [:clj]}))
@@ -16,7 +17,12 @@
                 :lib lib
                 :version version
                 :basis basis
-                :src-dirs ["src"]})
+                :src-dirs ["src"]
+                :pom-data [[:licenses
+                            [:license
+                             [:name "Eclipse Public License 1.0"]
+                             [:url "https://opensource.org/license/epl-1-0/"] ]]]})
+
   (b/copy-dir {:src-dirs ["src/clj"
                           "src/cljc"
                           "resources"]
@@ -33,3 +39,7 @@
               :class-dir class-dir})
   (println "Add to deps as:" lib "version" version))
 
+(defn deploy [_]
+  (dd/deploy {:installer :remote
+              :artifact jar-file
+              :pom-file (b/pom-path {:lib lib :class-dir class-dir})}))
